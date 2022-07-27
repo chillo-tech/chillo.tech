e8c35ecec94d56f6de4f2de9fd5ceb16b4139dfd
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2022-07-27 22:43:01 +0200

    Dockerfile

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ExpertiseCategory } from './ExpertiseCategory';
-export { default as Footer } from '../layouts/opened/Footer';
-export { default as Tabs } from './Tabs';
-export { default as Title } from './Title';
-
