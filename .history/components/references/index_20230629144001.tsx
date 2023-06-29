81ecd29840361c765f5d4a750eb524c47558a688
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-06-29 15:19:39 +0200

    Gestion des cookies

---
+++ .history/components/references/index_20230629144001.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+function GraphCMSImageLoader({ src, width }) {
+  const relativeSrc = (src) => src;
+
+  return `${process.env.BACKOFFICE_URL}${src}`;
+}
+function References() {
+  const [references, setReferences] = useState([]);
+  useEffect(() => {
+    const read = async () => {
+      try {
+        const response = await fetch(`${process.env.BACKOFFICE_URL}/chillo-services-customers`);
+        const data = await response.json();
+        setReferences(data);
+      } catch (error) {
+        console.log(error);
+        setReferences([]);
+      }
+    };
+    read();
+  }, []);
+  return (
+    <>
+      {references && references.length ? (
+        <div className="container mx-auto items-center text-center grid grid-cols-2 md:grid-cols-4 gap-4 references my-14">
+          {references.map((item, index) => (
+            <picture className="flex justify-center" key={`reference-${index}`}>
+              <source
+                srcSet={`${process.env.BACKOFFICE_URL}${item.image[0].url}`}
+                type="image/webp"
+              />
+              <img
+                src={`${process.env.BACKOFFICE_URL}${item.image[0].url}`}
+                alt={`${item.image[0].alternativeText}`}
+                className="image"
+              />
+            </picture>
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
+}
+
+export default References;
