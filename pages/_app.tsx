cec3d54dd640c43059b9dd225225c26ae422aa9b
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-07-18 06:31:14 +0200

    Google ananlytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import * as ga from '../lib'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import * as ga from '../lib';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Script from 'next/script';
 const configs = {
   defaultOptions: {
     queries: {
@@ -15,26 +16,45 @@ const configs = {
 };
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient(configs);
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      ga.pageview(url)
-    }
+      ga.pageview(url);
+    };
     //When the component is mounted, subscribe to router changes
     //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('routeChangeComplete', handleRouteChange);
 
     // If the component is unmounted, unsubscribe
     // from the event with the `off` method
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
-  return   (<QueryClientProvider client={queryClient}>
-    <Component {...pageProps} />
-  </QueryClientProvider>)
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload" id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
