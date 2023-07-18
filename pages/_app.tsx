bc554a92f21fe1cf2189812c5a5992c07e0b3430
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-07-18 06:47:39 +0200

    Google ananlytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,21 +35,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script strategy="lazyOnload" id="google-analytics">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
-      </Script>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
--- pages/coaching/index.tsx
+++ pages/coaching/index.tsx
@@ -31,13 +31,13 @@ function Coaching() {
           <section className="container mx-auto">
             <div className="card bg-white rounded-lg my-2 relative h-full">
               <ImageDisplay
-                imageClasses="object-cover overflow-hidden w-full h-full rounded-lg"
+                imageClasses="object-cover bottom-10 overflow-hidden w-full h-full rounded-lg"
                 wrapperClasses="absolute w-full h-full object-cover overflow-hidden rounded-lg border-2 border-blue-700"
                 image={data.image}
               />
-              <div className="relative py-16 md:px-10 px-4 flex items-start bg-slate-900/70 rounded-lg">
+              <div className="relative py-16 md:px-10 px-4 flex items-start bg-gray-900/30 rounded-lg">
                 <article className='md:w-1/2 z-50'>
-                  <RenderHtmlContent content={data.about} classes="md:text-3xl font-extralight text-slate-200 mb-12" />
+                  <RenderHtmlContent content={data.about} classes="md:text-3xl font-extralight text-white mb-12" />
                   <Link
                   href="https://calendly.com/chillo-tech/30min"
                   className="rounded-lg bg-blue-900 text-white md:text-xl text-sm flex-none font-extralight px-4 py-4"
